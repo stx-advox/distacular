1372a9c014d424057f17c84c93c43b849d3f9cba
hz <13457698+hozzjss@users.noreply.github.com>
2022-03-29 13:19:22 +0200

    fix server not showing static assets

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -6,13 +6,19 @@ const port = process.env.PORT || 8244;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "..", "client", "build")));
+console.log(process.env.NODE_ENV);
+
+const additional = process.env.NODE_ENV === "production" ? ".." : "";
+
+app.use(
+  express.static(path.join(__dirname, additional, "..", "client", "build"))
+);
 
 app.use("/api", apiRouter);
 
 app.use("*", function (request, response) {
   response.sendFile(
-    path.resolve(__dirname, "..", "client", "build", "index.html")
+    path.resolve(__dirname, additional, "..", "client", "build", "index.html")
   );
 });
 
