e070262117316cf937f7b2ea44603f2e19a5fb04
hz <13457698+hozzjss@users.noreply.github.com>
2022-03-30 09:33:21 +0200

    added contract address field

--- packages/server/bot/schemas/micro-dao.ts
+++ packages/server/bot/schemas/micro-dao.ts
@@ -3,11 +3,13 @@ import { Schema, model } from "mongoose";
 export interface IMicroDAO {
   name: string;
   members: string[];
+  contractAddress?: string;
 }
 
 const schema = new Schema<IMicroDAO>({
   name: { required: true, type: String },
   members: { required: true, type: [String] },
+  contractAddress: { type: String, required: false },
 });
 
 export const MicroDAO = model<IMicroDAO>("MicroDAO", schema);
